ddd5230d7a19ebdcc66f281943328a2869ad050b
nafisa <nafisatasnim8855@gmail.com>
2026-01-27 00:38:05 +0600

    Remove the user search method from auth controller to user controller
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -57,36 +57,4 @@ const login = async (req, res) => {
     }
 }
 
-const searchUsers = async (req, res) => {
-    try {
-        const { query } = req.query;
-
-        if (!query || query.trim() === '') {
-            return res.status(400).json({
-                message: "Search query is required",
-                success: false
-            });
-        }
-
-        // Search users by username or name (case-insensitive)
-        const users = await UserModel.find({
-            $or: [
-                { username: { $regex: query, $options: 'i' } },
-                { name: { $regex: query, $options: 'i' } }
-            ]
-        })
-            .select('name username profilePic bio')
-            .limit(20);
-
-        return res.status(200).json({
-            success: true,
-            count: users.length,
-            users
-        });
-    } catch (error) {
-        console.error('Search users error:', error);
-        res.status(500).json({ message: "Internal Server Error", success: false });
-    }
-}
-
-export { signup, login, searchUsers };
+export { signup, login };
